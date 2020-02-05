6000317f790634ea6f84fe0ea4568569a599bec4
EvanCroft666 <60704830+EvanCroft666@users.noreply.github.com>
2020-02-05 08:37:05 -0700

    Add files via upload

---
+++ WindowsFormsApplicationEvanC/WindowsFormsApplication2/FormDriverReg.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class FormDriverReg : Form
+    {
+        public FormDriverReg()
+        {
+            InitializeComponent();
+        }
+
+        public static FormDriverReg myD { get; internal set; }
+
+        private void ClickDriver(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void saveTextFrstNme(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void saveText(object sender, EventArgs e)
+        {
+            String Text1 = textBox1.Text;
+            String Text2 = textBox2.Text;
+            String Text3 = textBox3.Text;
+            String Text4 = textBox4.Text;
+
+
+            String bigString;
+            bigString = Text1.PadRight(15, ' ') + Text2.PadRight(15, ' ') + Text3.PadRight(1, ' ') + Text4.PadRight(10, ' ');
+
+
+            string path = @"MySmithDriver.txt";
+            string myNumbers = "12345678901234567890123456789012345678901";
+
+
+            if (!File.Exists(path))
+            {
+                using (System.IO.StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(myNumbers);
+                }
+
+            }
+            using (System.IO.StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(bigString);
+            }
+
+
+        }
+    }
+}
---
+++ WindowsFormsApplicationEvanC/WindowsFormsApplication2/FormMainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class FormMainMenu : Form
+    {
+
+        public FormMainMenu()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CloseApplication(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void ClickOpenDriver(object sender, EventArgs e)
+        {
+
+            FormDriverReg myD = new FormDriverReg();
+            myD.ShowDialog();
+        }
+    }
+}
